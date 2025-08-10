9c7d0e8f8f32361368d0d22fafd25c5c0e6f796f
Menaka J <menakaraman4@gmail.com>
2025-08-10 20:24:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from romania_map import bfs, dfs, ucs, romania_map
 import random
+import os
 
 app = Flask(__name__)
 
@@ -67,4 +68,6 @@ def get_route():
     })
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host="0.0.0.0", port=port, debug=True)
+
